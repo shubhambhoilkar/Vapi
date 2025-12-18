ce2a60ee2b08b8bb8097b987fdc0ad092265100b
Shubham Bhoilkar <91750972+shubhambhoilkar@users.noreply.github.com>
2025-12-18 17:47:10 +0530

    Modify outbound call script with status updates
    Updated the outbound call script to include random status updates and adjusted batch processing parameters.

--- update_working_vapi_outbound_call.py
+++ update_working_vapi_outbound_call.py
@@ -1,8 +1,8 @@
 import pandas as pd
-import os 
+import os , random
 import httpx
 import asyncio
-from datetime import datetime
+from datetime import datetime , timedelta
 from dotenv import load_dotenv
 
 #Load Dorenv
@@ -19,10 +19,13 @@ SHEET_NAME = "call_queue"
 # BATCH CONFIG
 BATCH_SIZE = 2          # 2 calls at same time
 TOTAL_BATCHES = 3       # 3 batches
-BATCH_DELAY_SECONDS = 30  # 30 seconds gap
+BATCH_DELAY_SECONDS = 10  # 10 seconds gap
+RETRY_GAP_HOURS = 24 # 24 Hours time gap
 
 # VAPI API CALL
 VAPI_URL = "https://api.vapi.ai/call/phone"
+
+DEV_RANDOM_STATUSES = ["inprogress", "failure", "success", "error", "no-response"]
 
 # VALIDATIONS
 if not VAPI_API_KEY or not ASSISTANT_ID or not PHONE_NUMBER_ID:
@@ -35,7 +38,6 @@ if not VAPI_API_KEY or not ASSISTANT_ID or not PHONE_NUMBER_ID:
 def normalize_phone(phone: str) -> str:
     try:
         phone = str(phone).strip()
-
         # Catch scientific notation early
         if "E+" in phone or "e+" in phone:
             raise ValueError(f"Invalid phone number from Excel: {phone}")
@@ -66,27 +68,33 @@ def is_valid_for_call(status_value) -> bool:
         print("Error in validating status. ", e )
 
 # LOAD EXCEL
-try:
-    df = pd.read_excel(EXCEL_FILE, sheet_name=SHEET_NAME, dtype=str)
+def load_excel():
+    return pd.read_excel(EXCEL_FILE, sheet_name=SHEET_NAME, dtype=str)
 
-    print("\n📄 Excel Data Read Successfully")
-    print(df)
-    print("-" * 60)
+def save_excel(df):
+    df.to_excel(EXCEL_FILE, sheet_name = SHEET_NAME, index = False)
 
-    queue_df = df[df["status"].apply(is_valid_for_call)].reset_index(drop=True)
+    
+# Dev: Async Status Update
+async def dev_update_status_async(sr_no):
+    await asyncio.sleep(random.randint(2,5))
 
-    if queue_df.empty:
-        print("No Valid records found for Call")
-        exit()
+    df = load_excel()
+    random_status = random.choice(DEV_RANDOM_STATUSES)
 
-    queue_df = queue_df.head(BATCH_SIZE * TOTAL_BATCHES)
+    df.loc[df["sr_no"].astype(str) == str(sr_no), "status"] = random_status
 
-    batches = [
-        queue_df.iloc[i:i + BATCH_SIZE]
-        for i in range(0, len(queue_df), BATCH_SIZE)
-    ]
-except Exception as e:
-    print("Fail to get the Data")
+    # Retry scheduling
+    if random_status in ["no-response", "failure", "error"]:
+        next_try = datetime.utcnow() + timedelta(hours = RETRY_GAP_HOURS)
+        df.loc[df["sr_no"].astype(str) == str(sr_no), "next_try"] = next_try.isoformat()
+
+        save_excel(df)
+        print(
+            f"DEV UPDATE | sr_no= {sr_no}"
+            f"status = {random_status}"
+        )
+        
 
 # MAKE SINGLE CALL
 async def make_call(rows):
@@ -98,7 +106,7 @@ async def make_call(rows):
         print("Name: ", rows["user_name"])
         print("Email: ", rows["email"])
         print("Phone: ", phone)
-        print("Time: ", datetime.utcnow().isoformat())
+        # print("Time: ", datetime.utcnow().isoformat())
 
         payload = {
             "assistantId": ASSISTANT_ID,
@@ -127,6 +135,9 @@ async def make_call(rows):
             print("Response JSON")
             print("Response received.\n",response.json())
 
+            # Dev Mode: 
+            asyncio.create_task(dev_update_status_async(rows["sr_no"]))
+
         except Exception as e:
             print("Exception while making call: ",str(e))
 
@@ -144,15 +155,39 @@ async def process_batch(batch_df, batch_number):
         tasks = []
         for _, row in batch_df.iterrows():
             tasks.append(make_call(row))
+        # tasks = [make_call(row) for _, row in batch_df.iterrows()]
 
         await asyncio.gather(*tasks)
-
         print(f"Batch {batch_number} completed")
+
     except Exception as e:
         print("Error in Batch Process. ", e )
 
 #MAIN EXECUTION
 async def main():
+    try:
+        df = load_excel()
+        print("\n📄 Excel Data Read Successfully")
+        print(df)
+        print("-" * 60)
+
+        # queue_df = df[df["status"].apply(is_valid_for_call)].reset_index(drop=True)
+        valid_df = df[df["status"].apply(is_valid_for_call)].reset_index(drop=True)
+        valid_df = valid_df.head(BATCH_SIZE * TOTAL_BATCHES)
+    
+        if valid_df.empty:
+            print("No Valid records found for Call")
+            exit()
+
+        # queue_df = queue_df.head(BATCH_SIZE * TOTAL_BATCHES)
+
+        batches = [
+            valid_df.iloc[i:i + BATCH_SIZE]
+            for i in range(0, len(valid_df), BATCH_SIZE)
+        ]
+    except Exception as e:
+        print("Fail to get the Data")
+
     try:
         for idx, batch in enumerate(batches[:TOTAL_BATCHES],start=1):
             await process_batch(batch, idx)
